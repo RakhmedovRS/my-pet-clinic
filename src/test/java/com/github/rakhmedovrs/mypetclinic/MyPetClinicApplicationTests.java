f17f8b9afdd2779e59a929ea18654f9482918e69
rassoll <rassoll32@gmail.com>
2020-04-29 23:27:48 +0300

    Initial commit

---
+++ src/test/java/com/github/rakhmedovrs/mypetclinic/MyPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.rakhmedovrs.mypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetClinicApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+}
