dbe6e552abed3938caab48daa77708013ca106ec
rassoll <rassoll32@gmail.com>
2020-05-07 21:57:29 +0300

    Added Index Page and controller. closes #8

---
+++ my-pet-clinic-web/src/main/java/com/github/rakhmedovrs/mypetclinic/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.github.rakhmedovrs.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author RakhmedovRS
+ * @created 07-May-20
+ */
+@Controller
+public class IndexController
+{
+	@RequestMapping(path = {"", "/", "index.html"}, method = RequestMethod.GET)
+	public String index()
+		{
+		return "index";
+	}
+}
