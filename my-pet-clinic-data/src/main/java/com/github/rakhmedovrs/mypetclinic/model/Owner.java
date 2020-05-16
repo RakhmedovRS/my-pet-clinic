9faa2ecf6db478fec686cb58014c8a5c8a4a1c6e
rassoll <rassoll32@gmail.com>
2020-05-16 20:21:42 +0300

    Added Visit updated Owner. closes #26

--- my-pet-clinic-data/src/main/java/com/github/rakhmedovrs/mypetclinic/model/Owner.java
+++ my-pet-clinic-data/src/main/java/com/github/rakhmedovrs/mypetclinic/model/Owner.java
@@ -1,9 +1,22 @@
 package com.github.rakhmedovrs.mypetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author RakhmedovRS
  * @created 03-May-20
  */
 public class Owner extends Person
 {
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets()
+	{
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets)
+	{
+		this.pets = pets;
+	}
 }
