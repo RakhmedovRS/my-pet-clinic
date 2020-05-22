c3c54c3e3b599c44eb081361812a1787f4bac372
rassoll <rassoll32@gmail.com>
2020-05-22 20:05:24 +0300

    Implemented BaseEntity .closes #35

--- my-pet-clinic-data/src/main/java/com/github/rakhmedovrs/mypetclinic/model/BaseEntity.java
+++ my-pet-clinic-data/src/main/java/com/github/rakhmedovrs/mypetclinic/model/BaseEntity.java
@@ -1,13 +1,20 @@
 package com.github.rakhmedovrs.mypetclinic.model;
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 /**
  * @author RakhmedovRS
  * @created 04-May-20
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId()
