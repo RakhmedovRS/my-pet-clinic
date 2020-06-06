5d6733e469a17b465aafc7d316faaa8fa05740af
rassoll <rassoll32@gmail.com>
2020-06-06 22:04:30 +0300

    Added InitBinder to protect id field from modification

--- my-pet-clinic-web/src/main/java/com/github/rakhmedovrs/mypetclinic/controllers/OwnerController.java
+++ my-pet-clinic-web/src/main/java/com/github/rakhmedovrs/mypetclinic/controllers/OwnerController.java
@@ -3,7 +3,9 @@ package com.github.rakhmedovrs.mypetclinic.controllers;
 import com.github.rakhmedovrs.mypetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +23,12 @@ public class OwnerController
 	public OwnerController(OwnerService ownerService)
 	{
 		this.ownerService = ownerService;
+	}
+
+	@InitBinder
+	public void setAllowedFields(WebDataBinder webDataBinder)
+	{
+		webDataBinder.setDisallowedFields("id");
 	}
 
 	@GetMapping(path = {"", "/", "/index", "/index.html"})
