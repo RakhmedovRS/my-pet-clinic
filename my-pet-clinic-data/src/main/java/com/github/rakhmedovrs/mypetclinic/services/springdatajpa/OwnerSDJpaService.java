9cc8b82f298963ee4cbbd5ae97ce071ebc3f9f75
rassoll <rassoll32@gmail.com>
2020-06-07 21:33:02 +0300

    Fixed find by like. closes #62

--- my-pet-clinic-data/src/main/java/com/github/rakhmedovrs/mypetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ my-pet-clinic-data/src/main/java/com/github/rakhmedovrs/mypetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -72,6 +72,6 @@ public class OwnerSDJpaService implements OwnerService
 	@Override
 	public List<Owner> findAllByLastNameLike(String lastName)
 	{
-		return ownerRepository.findAllByLastNameLike(lastName);
+		return ownerRepository.findAllByLastNameLike("%"+lastName+"%");
 	}
 }
